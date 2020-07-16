29177e5a41cbb7b93cf41132bd43c10a79c47810
Amy Beisel <amy.beisel@gmail.com>
2020-07-16 12:22:20 -0600

    corrected

--- src/room.py
+++ src/room.py
@@ -1,6 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+
+
 class Room:
     def __init__(self, name, description):
         self.name = name
@@ -10,7 +12,8 @@ class Room:
         self.e_to = None
         self.w_to = None
 
-    # def __str__(self):
-    # return self.name + '\n' +self.description
+    def __str__(self):
+        return f'Room: {self.name} , description: {self.description}'
+
     
         
